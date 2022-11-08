f469a084c85621d51528d61850b421b9bbbe9d5a
Mohit Soni <mohitsoni2111@gmail.com>
2022-11-08 11:35:14 +0530

    heroku files added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tirupati_impex.wsgi
--- tirupati_impex/settings.py
+++ tirupati_impex/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -126,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
